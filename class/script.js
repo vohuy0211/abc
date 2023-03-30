069949daace0531fd3a4402d59f968fc50cf2fc4
vohuy0211 <114748297+vohuy0211@users.noreply.github.com>
2023-03-30 09:20:43 +0700

    baitap

---
+++ class/script.js
@@ -0,0 +1,6 @@
+// alert("script đã được nhúng vào")
+let value = prompt("Nhập vào tuổi của các bạn");
+
+console.log("Tuổi nhập vào là :", value);
+console.log("nhập năm sinh của bạn",prompt("nhập năm sinh"));
+console.log('Bạn đồng ý làm người yêu tôi không ?',confirm('bạn đồng ý làm người yêu tôi không ?'));
