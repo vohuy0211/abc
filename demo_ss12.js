ec12883b60ef9f6d30f154b433bab0d611bb19a0
vohuy0211 <114748297+vohuy0211@users.noreply.github.com>
2023-04-06 08:16:00 +0700

    baitap

---
+++ demo_ss12.js
@@ -0,0 +1,18 @@
+// demo vòng lặp 
+
+// lặp 10 lần
+
+// for(let i = 1 ; i <= 10 ; i = i + 1){
+//     console.log("lặp lần thứ :", i);
+// }
+
+
+// Nhập vào N vào chuỗi số và tìm ra số lớn nhất
+
+// Chuỗi số :a1 ,a2,a3,a4,...AbortController.......
+// let max = a1
+// for (let i = 1; i < navigator; i = i+1){
+//     if(ai >= a1) {
+//         max = ai
+//     }
+// }console.log('Số lớn nhất trong chuỗi' , max);
